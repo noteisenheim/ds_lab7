180790a9b3396000764a38e1c9e11fbc1b275f58
noteisenheim <noteisenheim@gmail.com>
2020-09-23 18:21:03 +0300

    add screenshots

--- app.py
+++ app.py
@@ -5,23 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    frogs = ['bullfrog', 'wood frog', 'cane toad', 'squeeking frog']
-    frog = random.choice(frogs)
 
-    return render_template('index.html', frog=frog)
+    return render_template('index.html')
 
 @app.route('/other')
 def other():
     return render_template('other.html')
-
-@app.route('/signup')
-def signup():
-    return render_template('signin.html')
-
-@app.route('/thanks')
-def thanks():
-    name = request.args.get('name')
-    return render_template('thanks.html', name=name)
-
 if __name__ == '__main__':
     app.run(port=5000, host='0.0.0.0')
